8f2ef02032f37f9cc371e648a4d4bb36cc2dcb8d
devbrius <161338563+briusxD@users.noreply.github.com>
2024-07-10 18:54:25 +0000

    Pending changes exported from your codespace

---
+++ src/main/java/org/jetbrains/annotations/NotNull.java
@@ -0,0 +1,11 @@
+package org.jetbrains.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
+public @interface NotNull {
+}
